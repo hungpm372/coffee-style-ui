6163cd6e7e6c9896c0e353933bfaba5155f2f1cd
Phan Minh Hung <hungpm372@gmail.com>
2024-02-09 20:17:13 +0700

    create story section

--- tailwind.config.js
+++ tailwind.config.js
@@ -7,7 +7,12 @@ module.exports = {
         Karla: ['Karla', 'sans-serif']
       },
       colors: {
-        "light-coffee": "#C89F94"
+        "coffee": {
+          50: "#e8d6d0",
+          200: "#C89F94",
+          400: '#a25f4b',
+          600: "#744838",
+        }
       },
       keyframes: {
         slideDown: {
